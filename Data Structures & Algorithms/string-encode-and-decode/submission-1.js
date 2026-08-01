6ef311ffadb6472a5f569ffc7020f427ca27a0b0
Curran Doddabele <44831891+Curran-C@users.noreply.github.com>
2026-08-01 09:42:57 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    delimiter = "~"
+    encode(strs) {
+        return strs.reduce((acc, each) => {
+            acc = acc + each.length + this.delimiter + each
+            return acc 
+        }, "")
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i;
+
+            while (str[j] !== this.delimiter) {
+                j++;
+            }
+
+            const len = parseInt(str.slice(i, j));
+
+            res.push(str.slice(j + 1, j + 1 + len));
+
+            i = j + 1 + len;
+        }
+
+        return res;
+    }
+}
